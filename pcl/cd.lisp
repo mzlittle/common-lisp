0c53a805485d5defcc053b95d9f716f537afaa87
Zach Little <zach@rightscale.com>
2014-02-15 09:22:12 -0800

    finished ui

--- pcl/cd.lisp
+++ pcl/cd.lisp
@@ -3,19 +3,41 @@
     :title title 
     :artist artist 
     :rating rating 
-    :ripped ripped)
-   )
+    :ripped ripped))
 
 (defvar *db* nil)
 
 (defun add-record (cd) 
   (push cd *db*))
 
-; (add-record (make-cd "alone" "mad season" 9 t))
-; (add-record (make-cd "Roses" "Kathy Mattea" 7 t))
-; (add-record (make-cd "Fly" "Dixie Chicks" 8 t))
-; (add-record (make-cd "Home" "Dixie Chicks" 9 t))
+(defun populate-db ()
+  (add-record (make-cd "alone" "mad season" 9 t))
+  (add-record (make-cd "Roses" "Kathy Mattea" 7 t))
+  (add-record (make-cd "Fly" "Dixie Chicks" 8 t))
+  (add-record (make-cd "Home" "Dixie Chicks" 9 t))
+  (add-record (make-cd "Rockin the burbs" "ben folds" 4 t))
+  (add-record (make-cd "give us a break" "limpopo" 8 t))
+  (add-record (make-cd "Lyle Lovett" "Lyle Lovett" 9 t)))
 
 (defun db-dump ()
   (dolist (cd *db*)
     (format t "~{~a:~8t~a~%~}~%" cd)))
+
+(defun dump-db2 ()
+  (format t "~{~{~a:~10t~a~%~}~%~}" *db*))
+
+(defun prompt-read (prompt)
+  (format *query-io* "~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+(defun prompt-for-cd ()
+  (make-cd
+   (prompt-read "Title")
+   (prompt-read "Artist")
+   (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+   (y-or-n-p "Ripped [y/n]: ")))
+
+(defun add-cds ()
+  (loop (add-record (prompt-for-cd))
+    (if (not (y-or-n-p "Another? [y/n]: ")) (return))))
