22acfafd4cd07e20389da1b43f14f9f7650e0c18
Zach Little <zach@rightscale.com>
2014-02-15 11:39:15 -0800

    add search by artist

--- pcl/cd.lisp
+++ pcl/cd.lisp
@@ -1,4 +1,14 @@
- (defun make-cd (title artist rating ripped)
+
+(defun populate-db ()
+  (add-record (make-cd "alone" "mad season" 9 t))
+  (add-record (make-cd "Roses" "Kathy Mattea" 7 t))
+  (add-record (make-cd "Fly" "Dixie Chicks" 8 t))
+  (add-record (make-cd "Home" "Dixie Chicks" 9 t))
+  (add-record (make-cd "Rockin the burbs" "ben folds" 4 t))
+  (add-record (make-cd "give us a break" "limpopo" 8 t))
+  (add-record (make-cd "Lyle Lovett" "Lyle Lovett" 9 t)))
+
+(defun make-cd (title artist rating ripped)
    (list 
     :title title 
     :artist artist 
@@ -9,15 +19,6 @@
 
 (defun add-record (cd) 
   (push cd *db*))
-
-(defun populate-db ()
-  (add-record (make-cd "alone" "mad season" 9 t))
-  (add-record (make-cd "Roses" "Kathy Mattea" 7 t))
-  (add-record (make-cd "Fly" "Dixie Chicks" 8 t))
-  (add-record (make-cd "Home" "Dixie Chicks" 9 t))
-  (add-record (make-cd "Rockin the burbs" "ben folds" 4 t))
-  (add-record (make-cd "give us a break" "limpopo" 8 t))
-  (add-record (make-cd "Lyle Lovett" "Lyle Lovett" 9 t)))
 
 (defun db-dump ()
   (dolist (cd *db*)
@@ -41,3 +42,23 @@
 (defun add-cds ()
   (loop (add-record (prompt-for-cd))
     (if (not (y-or-n-p "Another? [y/n]: ")) (return))))
+
+(defun save-db (filename)
+  (with-open-file (out filename
+		   :direction :output
+		   :if-exists :supersede)
+    (with-standard-io-syntax
+      (print *db* out))))
+
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax
+      (setf *db* (read in)))))
+
+(defun select-by-artist (artist)
+  (remove-if-not
+   #'(lambda (cd) (equal (getf cd :artist) artist)) 
+   *db*))
+
+(defun select (selector-fn)
+  (remove-if-not selector-fn *db*))
